33917981f04503b3320664c10e2f9d9113e06548
yousra <yousra.chaieb@esprit.tn>
2023-10-23 19:36:12 +0100

    second commit

---
+++ src/Zoo.java
@@ -0,0 +1,73 @@
+public class Zoo {
+
+    static final int NUMBER_OF_CAGES = 25;
+    Animal[] animals;
+    String name, city;
+    int nbrAnimals;
+
+    public Zoo() {
+    }
+
+    public Zoo(String name, String city) {
+        animals = new Animal[NUMBER_OF_CAGES];
+        this.name = name;
+        this.city = city;
+    }
+
+    static Zoo comparerZoo(Zoo z1, Zoo z2) {
+        if (z1.nbrAnimals > z2.nbrAnimals)
+            return z1;
+        return z2;
+    }
+
+    void displayZoo() {
+        System.out.println("Name: " + name + ", City: " + city + ", N° Cages: " + NUMBER_OF_CAGES + " N° animals: " + nbrAnimals);
+    }
+
+    boolean addAnimal(Animal animal) {
+        if (searchAnimal(animal) != -1)
+            return false;
+        if (nbrAnimals == NUMBER_OF_CAGES)
+            return false;
+        animals[nbrAnimals] = animal;
+        nbrAnimals++;
+        return true;
+    }
+
+    boolean removeAnimal(Animal animal) {
+        int indexAnimal = searchAnimal(animal);
+        if (indexAnimal == -1)
+            return false;
+        for (int i = indexAnimal; i < nbrAnimals; i++) {
+            animals[i] = animals[i + 1];
+        }
+        animals[nbrAnimals] = null;
+        this.nbrAnimals--;
+        return true;
+    }
+
+    void displayAnimals() {
+        System.out.println("List of animals of " + name + ":");
+        for (int i = 0; i < nbrAnimals; i++) {
+            System.out.println(animals[i]);
+        }
+    }
+
+    int searchAnimal(Animal animal) {
+        int index = -1;
+        for (int i = 0; i < nbrAnimals; i++) {
+            if (animal.name == animals[i].name)
+                return i;
+        }
+        return index;
+    }
+
+    boolean isZooFull() {
+        return nbrAnimals == NUMBER_OF_CAGES;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", City: " + city + ", N° Cages: " + NUMBER_OF_CAGES + " N° animals: " + nbrAnimals;
+    }
+}
